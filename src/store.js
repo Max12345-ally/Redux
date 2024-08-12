b7c6445a772a827de34839ed6a680a800af615b7
Max12345-ally <ceo@rf.ru>
2024-08-12 23:54:23 +0300

    store completed

--- src/store.js
+++ src/store.js
@@ -14,7 +14,7 @@ function reducer(state = initialState, action) {
             return {...state, balance: state.balance - action.payload};
         case "account/requestLoan":
             if(state.loan > 0) return state;
-            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose}    
+            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount}    
         case 'account/payLoan':
             return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan}
 
